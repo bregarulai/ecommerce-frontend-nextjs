335ce70ca28f2ac9d0dcd59eb699145dd205ca44
bregarulai <bregarulai@gmail.com>
2022-04-26 19:40:15 -0400

    added dynamic data to link and h1

--- components/HeroBanner/HeroBanner.jsx
+++ components/HeroBanner/HeroBanner.jsx
@@ -5,16 +5,18 @@ import { urlFor } from '../../lib/client';
 import styles from './heroBanner.module.scss';
 
 const HeroBanner = ({ bannerInfo }) => {
-  const { smallText, midText, buttonText, desc, image } = bannerInfo;
+  const { smallText, midText, buttonText, desc, image, largeText1, product } =
+    bannerInfo;
   const { container, beatsSolo, imageContainer, description } = styles;
   return (
     <div className={container}>
       <div>
         <p className={beatsSolo}>{smallText}</p>
         <h3>{midText}</h3>
+        <h1>{largeText1}</h1>
         <img src={urlFor(image)} alt='headphones' className={imageContainer} />
         <div>
-          <Link href='/product/id'>
+          <Link href={`/product/${product}`}>
             <button type='button'>{buttonText}</button>
           </Link>
           <div className={description}>
