6b35812feccdddbe028f21b85ab56bc16e5fa55b
bregarulai <bregarulai@gmail.com>
2022-04-26 14:10:48 -0400

    created starting structure for index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 const Home = () => {
   return (
-    <div>
-      <h2>Hello Ecommerce</h2>
-    </div>
+    <>
+      Hero Banner Component
+      <div>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div>{['product 1', 'product 2'].map((product) => product)}</div>
+      Footer
+    </>
   );
 };
 
