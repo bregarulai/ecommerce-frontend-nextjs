dc12b30456962b44b0d56385b8f98099d646d6ea
bregarulai <bregarulai@gmail.com>
2022-04-26 19:58:59 -0400

    created image section

--- components/Product/Product.jsx
+++ components/Product/Product.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import Link from 'next/link';
 
+import { urlFor } from '../../lib/client';
 import styles from './product.module.scss';
 
-const Product = () => {
-  const {} = styles;
-  return <div>Product</div>;
+const Product = ({ product }) => {
+  console.log(product);
+  const { name, image, price, details, slug } = product;
+  const { card, imageContainer } = styles;
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className={card}>
+          <img
+            src={urlFor(image && image[0])}
+            alt={name}
+            width={250}
+            height={250}
+            className={imageContainer}
+          />
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Product;
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,9 @@ const Home = ({ products, bannerData }) => {
         <p>Speakers of many variations</p>
       </div>
       <div className={products__container}>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
       <FooterBanner />
     </>
