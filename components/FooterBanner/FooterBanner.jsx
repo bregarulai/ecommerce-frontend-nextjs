9aab417c9dcee36bdafcbc9d0d70847e46c40147
bregarulai <bregarulai@gmail.com>
2022-04-29 12:58:28 -0400

    added data to lef container

--- components/FooterBanner/FooterBanner.jsx
+++ components/FooterBanner/FooterBanner.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 import styles from './footerBanner.module.scss';
 
-const FooterBanner = () => {
+const FooterBanner = ({ bannerData }) => {
+  const { discount, largeText1, largeText2, saleTime } = bannerData;
   const { container, description, left, right } = styles;
   return (
     <div className={container}>
       <div className={description}>
-        <div className={left}></div>
+        <div className={left}>
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
         <div className={right}></div>
       </div>
     </div>
