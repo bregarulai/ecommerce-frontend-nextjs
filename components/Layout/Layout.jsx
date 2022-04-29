b631f112b765fc8eef2c769b258db287d4ecf151
bregarulai <bregarulai@gmail.com>
2022-04-29 13:50:52 -0400

    created layout component

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Head from 'next/head';
 
+import { Navbar, Footer } from '../../components';
 import styles from './layout.module.scss';
 
-const Layout = () => {
-  const {} = styles;
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  const { container, mainContainer } = styles;
+  return (
+    <div className={container}>
+      <Head>
+        <title>Bregacode Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className={mainContainer}>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
