557c7579ea50a3f399232bbc0d0e9be73e8e8e86
bregarulai <bregarulai@gmail.com>
2022-04-29 14:14:32 -0400

    implemented footer component

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 
 import styles from './footer.module.scss';
 
 const Footer = () => {
-  const {} = styles;
-  return <div>Footer</div>;
+  const { container, icons } = styles;
+  return (
+    <div className={container}>
+      <p>2022 Bregacode Heaphones All rights reserved</p>
+      <p className={icons}>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
