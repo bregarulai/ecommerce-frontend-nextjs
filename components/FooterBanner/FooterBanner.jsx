2d546cbad5782cd03c9b5e369e3c64a44ab9dee9
bregarulai <bregarulai@gmail.com>
2022-04-29 13:37:17 -0400

    added media query

--- components/FooterBanner/FooterBanner.jsx
+++ components/FooterBanner/FooterBanner.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Link from 'next/link';
 
+import { urlFor } from '../../lib/client';
 import styles from './footerBanner.module.scss';
 
 const FooterBanner = ({ bannerData }) => {
-  const { discount, largeText1, largeText2, saleTime } = bannerData;
-  const { container, description, left, right } = styles;
+  const {
+    discount,
+    largeText1,
+    largeText2,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    product,
+    buttonText,
+    image,
+  } = bannerData;
+  const { container, description, left, right, footerImg } = styles;
   return (
     <div className={container}>
       <div className={description}>
@@ -14,7 +27,15 @@ const FooterBanner = ({ bannerData }) => {
           <h3>{largeText2}</h3>
           <p>{saleTime}</p>
         </div>
-        <div className={right}></div>
+        <div className={right}>
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type='button'>{buttonText}</button>
+          </Link>
+        </div>
+        <img src={urlFor(image)} alt='footer' className={footerImg} />
       </div>
     </div>
   );
