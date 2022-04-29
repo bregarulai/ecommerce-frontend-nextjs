9ec8eec7494171e09bb823ae839c3d2b03da2b1a
bregarulai <bregarulai@gmail.com>
2022-04-29 14:06:32 -0400

    implemented navbar component

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 import styles from './navbar.module.scss';
 
 const Navbar = () => {
-  const {} = styles;
-  return <div>Navbar</div>;
+  const { container, logo, cartIcon, cartItemQty } = styles;
+  return (
+    <div className={container}>
+      <p className={logo}>
+        <Link href='/'>Bregacode Headphones</Link>
+      </p>
+      <button type='button' className={cartIcon}>
+        <AiOutlineShopping />
+        <span className={cartItemQty}>1</span>
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
