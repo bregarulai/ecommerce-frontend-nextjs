e920b0a52b72b84e73c9d9829db1971e5af2ac64
bregarulai <bregarulai@gmail.com>
2022-05-03 19:01:49 -0400

    cart component showing when cart icon is clicked

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -3,18 +3,26 @@ import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai';
 
 import styles from './navbar.module.scss';
+import { Cart } from '../../components';
+import { useAppContext } from '../../context/appContext';
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuatities } = useAppContext();
   const { container, logo, cartIcon, cartItemQty } = styles;
   return (
     <div className={container}>
       <p className={logo}>
         <Link href='/'>Bregacode Headphones</Link>
       </p>
-      <button type='button' className={cartIcon}>
+      <button
+        type='button'
+        className={cartIcon}
+        onClick={() => setShowCart(true)}
+      >
         <AiOutlineShopping />
-        <span className={cartItemQty}>1</span>
+        <span className={cartItemQty}>{totalQuatities}</span>
       </button>
+      {showCart && <Cart />}
     </div>
   );
 };
