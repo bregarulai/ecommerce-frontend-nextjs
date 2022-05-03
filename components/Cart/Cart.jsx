df771edb24ec874cde83e1d4db19c7f5ec134037
bregarulai <bregarulai@gmail.com>
2022-05-03 19:12:30 -0400

    added needed imports and items from context

--- components/Cart/Cart.jsx
+++ components/Cart/Cart.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import { toast } from 'react-hot-toast';
 
+import { useAppContext } from '../../context/appContext';
+import { urlFor } from '../../lib/client';
 import styles from './cart.module.scss';
 
 const Cart = () => {
   const {} = styles;
+  const cartRef = useRef();
+  const { totalPrice, totoalQuantities, cartItems, setShowCart } =
+    useAppContext();
   return <div>Cart</div>;
 };
 
