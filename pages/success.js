7e77343ea2f4ea0824faed268eab1a624f5975ec
bregarulai <bregarulai@gmail.com>
2022-05-04 17:42:35 -0400

    created email message to success page

--- pages/success.js
+++ pages/success.js
@@ -1,20 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { BsBagCheckFill } from 'react-icons/bs';
-import { useRouter } from 'next/router';
 
 import { useAppContext } from '../context/appContext';
 import styles from '../styles/success.module.scss';
 
 const Success = () => {
-  const { successWrapper, success, icon } = styles;
+  const { successWrapper, success, icon, emailMsg, description, email } =
+    styles;
   const { setCartItems, setTotalPrice, setTotalQuantities } = useAppContext();
-  const [order, setOrder] = useState(null);
+
   return (
     <div className={successWrapper}>
       <div className={success}>
         <p className={icon}>
           <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className={emailMsg}>Check your email inbox for your receipt</p>
+        <p className={description}>
+          If you have any question please email{' '}
+          <a className={email} href='mailto:order@example.com'>
+            order@example.com
+          </a>
         </p>
       </div>
     </div>
