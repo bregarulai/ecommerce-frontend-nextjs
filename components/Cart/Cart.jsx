bf1d0faff3b711b5222640a2753e6be3798088c5
bregarulai <bregarulai@gmail.com>
2022-05-03 20:12:14 -0400

    added item price to cart component

--- components/Cart/Cart.jsx
+++ components/Cart/Cart.jsx
@@ -72,6 +72,7 @@ const Cart = () => {
                 <div className={itemDesc}>
                   <div className={`${flex} ${top}`}>
                     <h5>{item.name}</h5>
+                    <h4>${item.price}</h4>
                   </div>
                 </div>
               </div>
